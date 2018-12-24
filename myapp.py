74d49295deda42b9e844cac0e5eebc329ccc4e5c
Simon Frisch <simon.frisch@gmail.com>
2018-12-24 14:58:07 -0600

    adding api functionality

--- myapp.py
+++ myapp.py
@@ -1,13 +1,23 @@
 from flask import Flask,render_template,request
+from flask_restful import Resource,Api
+
 application = Flask(__name__)
+api = Api(application)
 
-@application.route("/")
-def main():
+#@application.route("/")
+#def main():
    
-    return render_template('index.html')
+#    return render_template('index.html')
 
+class apipred(Resource):
+     def post(self):
+          data = request.get_json()
+          name = data['name']
+          name = name + "!!!"
+          return name
 
+api.add_resource(apipred,'/apipred/')
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0',debug=True)
 
